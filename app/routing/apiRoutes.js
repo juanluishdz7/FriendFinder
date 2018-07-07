bf7c6a52c9f1cc01aa326315f8c4142f17860ab8
Juan Luis Hernandez <juanluishdz85@gmail.com>
2018-07-07 11:51:31 -0500

    almost

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,23 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    
+    var userInput = req.body;
+    var userAns = userInput.scores;
+    var matchName = '';
+    var matchImage = '';
+    var totalDiff = 10000;
+    for (var i = 0; i < friends.length; i++) {
+      var diff = 0;
+      for (var j = 0; j < userAns.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userAns[j]);
+      }
+      if (diff < totalDiff) {
+        totalDiff = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
+    friends.push(userInput);
+    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
   })
 };
